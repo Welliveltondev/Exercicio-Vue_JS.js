b9017eacbdd66087a4a23da522549f8e809b6a6b
Wellivelton Fonseca <welliveltonfonseca@gmail.com>
2024-06-24 11:41:29 -0300

    modificação total da pag

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,52 +1,15 @@
 <script setup>
-import { reactive } from 'vue';
-const primeiroNUmero = document.getElementById("operacaoUm");
-const segundoNUmero = document.getElementById("operacaoDois");
+import Calculator from './components/Calculator.vue'
 
-const estado = reactive({
-  adicao: '+',
-  subtracao: '-',
-  divisao: '/',
-  multiplicacao: '*'
-})
-
-function calculadora() {
-  return primeiroNUmero, segundoNUmero;
-}
-
-calculadora();
-//javascript
 </script>
 
 <template> 
-<!-- html -->
-<div class="container">
-  <h1>Calculadora</h1>
-<div>
-  <p>Escolha a operação desejada</p>
-    <select>
-      <option value="adicao" selected>+
-        {{ estado.adicao }}
-      </option>
-      <option value="subtracao">-
-        {{ estado.subtracao }}
-      </option>
-      <option value="divisao">/
-        {{ estado.divisao }}
-      </option>
-      <option value="multiplicacao">*
-        {{ estado.multiplicacao }}
-      </option>
-    </select>
-</div>
- <div id="calc">
-  <input id="operacaoUm" type="number" min="1" placeholder="Digite o primeiro número">
-  <span id="operador"></span>
-  <input id="operacaoDois" type="number" min="0" placeholder="Digite o segundo número">
-</div>
+<div id="app">
+  <Calculator/>
 </div>
 
 </template>
+
 
 <style scoped>
 </style>
